91157040efce7bfae415211f5a09d058facacaa4
Danil <danilcaheleh@gmail.com>
2023-12-20 08:45:26 +0300

    Проект готов

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(name, link, cardDelete) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__image').alt = name;
+    cardElement.querySelector('.card__title').textContent = name;
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+    cardDeleteButton.addEventListener("click" , () => cardDelete(cardElement));
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
+const deleteCard = function (cardElement) {
+    cardElement.remove();
+};
+
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (Element) {
+    placesList.append(createCard(Element.name, Element.link, deleteCard));
+});
