6994515f4b7f0482805797c793ed926ffaa897e8
Danil <danilcaheleh@gmail.com>
2023-12-21 03:07:49 +0300

    Доработки

--- scripts/index.js
+++ scripts/index.js
@@ -7,8 +7,9 @@ const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
 function addCard(name, link, deleteCard) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    const getImage = cardElement.querySelector('.card__image').src = link;
-    getImage.alt = name;
+    const cardImage = cardElement.querySelector('.card__image')
+    cardImage.src = link;
+    cardImage.alt = name;
     cardElement.querySelector('.card__title').textContent = name;
     const cardDeleteButton = cardElement.querySelector('.card__delete-button');
     cardDeleteButton.addEventListener("click" , () => deleteCard(cardElement));
